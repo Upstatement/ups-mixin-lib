9cc56681c9b8c2c3b524d65ce45fbd0f7ab8fac8
Mike Burns <mike.burns@upstatement.com>
2016-04-30 13:17:22 -0400

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "Why no worky?"
-env
+cmd=$(npm run-script release-docs 2>&1 | tail -1)
 
-# cmd=$(npm run release-docs 2>&1 | tail -1)
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 
-# if [ $? -ne 0 ]; then
-#   exit 1
-# fi
-
-# eval $cmd
+eval $cmd
