f63a43719b1c013283e3e95f28f637f456cf59a5
Mike Burns <mike.burns@upstatement.com>
2016-05-24 15:51:27 -0400

    Un-commenting lines in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,14 +9,14 @@ if [ -n "$GIT_AUTHOR_NAME" ]; then
   git config --global user.name "$GIT_AUTHOR_NAME"
 fi
 
-echo "Preparing to deploy as  `git config user.name` <`git config user.email`>"
+echo "Preparing to deploy as  `git config --global user.name` <`git config --globaluser.email`>"
 
 # Run `sassdocify`, store last line of output (`git push` command)
-# cmd=$(npm run release-docs 2>&1 | tail -1)
+cmd=$(npm run release-docs 2>&1 | tail -1)
 
-# if [ $? -ne 0 ]; then
-#   exit 1
-# fi
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 
 # Execute `git push`
-# eval $cmd
+eval $cmd
