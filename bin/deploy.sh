57933ccb1ae68e88059fcae59319bece59746bc9
Mike Burns <mike.burns@upstatement.com>
2016-05-24 15:49:06 -0400

    Final finalization of deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-if [ -z "$GIT_AUTHOR_EMAIL" ]; then
+# Set global Git user config from environment variables if set
+if [ -n "$GIT_AUTHOR_EMAIL" ]; then
   git config --global user.email "$GIT_AUTHOR_EMAIL"
 fi
 
-if [ -z "$GIT_AUTHOR_NAME" ]; then
+if [ -n "$GIT_AUTHOR_NAME" ]; then
   git config --global user.name "$GIT_AUTHOR_NAME"
 fi
 
-cmd=$(npm run release-docs 2>&1 | tail -1)
+echo "Preparing to deploy as  `git config user.name` <`git config user.email`>"
 
-if [ $? -ne 0 ]; then
-  echo "$cmd"
-  exit 1
-fi
+# Run `sassdocify`, store last line of output (`git push` command)
+# cmd=$(npm run release-docs 2>&1 | tail -1)
 
-eval $cmd
+# if [ $? -ne 0 ]; then
+#   exit 1
+# fi
+
+# Execute `git push`
+# eval $cmd
