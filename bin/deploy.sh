44237ca9a95b8eae93f59444ca420d043cae0b6e
Mike Burns <mike.burns@upstatement.com>
2016-05-24 15:39:49 -0400

    Finalize deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-git config --global user.email "$GIT_AUTHOR_EMAIL"
-git config --global user.name "$GIT_AUTHOR_NAME"
+if [ -z "$GIT_AUTHOR_EMAIL" ]; then
+  git config --global user.email "$GIT_AUTHOR_EMAIL"
+fi
 
-npm run release-docs
+if [ -z "$GIT_AUTHOR_NAME" ]; then
+  git config --global user.name "$GIT_AUTHOR_NAME"
+fi
 
-# cmd=$(npm run release-docs 2>&1 | tail -1)
+cmd=$(npm run release-docs 2>&1 | tail -1)
 
-# if [ $? -ne 0 ]; then
-#   echo "$cmd"
-#   exit 1
-# fi
+if [ $? -ne 0 ]; then
+  echo "$cmd"
+  exit 1
+fi
 
-# eval $cmd
+eval $cmd
